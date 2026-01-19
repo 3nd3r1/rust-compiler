368a85c08857ca5e8142a4391221f017973bda3e
3nd3r1 <ender.spam.guru@gmail.com>
2026-01-19 11:19:56 +0200

    feat(setup): create empty functions from the python example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello world");
 }
+
+fn run_server(host: &str, port: u16) {
+    todo!()
+}
+
+fn call_compiler(source_code: &str) -> Result<Vec<u8>, String> {
+    todo!()
+}
