6da0aa1054b11db3f10891cd455a0c8d790f63d7
3nd3r1 <ender.spam.guru@gmail.com>
2026-02-16 16:00:36 +0200

    feat(type_checker): add display for types

--- src/compiler/type_checker.rs
+++ src/compiler/type_checker.rs
@@ -13,6 +13,24 @@ pub mod types {
             return_type: Box<Type>,
         },
     }
+
+    impl std::fmt::Display for Type {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            let s = match self {
+                Type::Int => "Int".to_string(),
+                Type::Bool => "Bool".to_string(),
+                Type::Unit => "Unit".to_string(),
+                Type::Function {
+                    params,
+                    return_type,
+                } => {
+                    let params_str: Vec<String> = params.iter().map(|p| format!("{}", p)).collect();
+                    format!("({}) -> {}", params_str.join(", "), *return_type)
+                }
+            };
+            write!(f, "{}", s)
+        }
+    }
 }
 
 use types::Type;
